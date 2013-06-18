05c3559602265d6d09ef02dfb9a80d50a0b9f4ec
Ronan Guilloux <ronan.guilloux@gmail.com>
2013-06-18 09:37:12 +0200

    tutorial enhancements for postgresql newbies: dsn parameters + READMEing about install

--- bootstrap.php
+++ bootstrap.php
@@ -1,14 +1,23 @@
 <?php // bootstrap.php
 
+$parameters = array(
+    'login'=> 'elcaro',
+    'password' => 'elcaro',
+    'host' => 'localhost',
+    'port' => 5432,
+    'dbname' => 'ElCaro'
+);
+
 $loader = require __DIR__."/vendor/autoload.php";
 $loader->add(null, __DIR__."/lib");
 
-$database = new Pomm\Connection\Database(array('dsn' => 'pgsql://greg/greg', 'name' => 'el_caro'));
+$database = new Pomm\Connection\Database(array('dsn' => sprintf("pgsql://%s:%s@%s:%d/%s",
+    $parameters['login'], $parameters['password'], $parameters['host'], $parameters['port'], $parameters['dbname'])));
 
 return $database
       ->registerConverter(
-          'Department', 
-          new \Pomm\Converter\PgEntity($database->getConnection()->getMapFor('\ElCaro\Company\Department')), 
+          'Department',
+          new \Pomm\Converter\PgEntity($database->getConnection()->getMapFor('\ElCaro\Company\Department')),
           array('company.department')
       )
       ->getConnection();
